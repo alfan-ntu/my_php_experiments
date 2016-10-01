664d79f8de79bcd9680754746dd285ffe6cb98c5
alfan-ntu <maoyi_fan@yahoo.com.tw>
2016-10-01 18:55:21 +0800

    Get external variables from a FORM

---
+++ demo0404.php
@@ -0,0 +1,3 @@
+<?php
+$shop_name = "Al's Cafe Shop";
+echo "{$_GET['user_name']}，Welcome to {$shop_name}！";
---
+++ demo0405.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Visitor Log In</title>
+  </head>
+  <body>
+    <form class="form" action="demo0404.php" method="get">
+      Visitor Name：
+      <input type="text" name="user_name" placeholder="Visitor's Name">
+    </form>
+  </body>
+</html>
