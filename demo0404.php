a667e99836e34064011863110b67019c5a68a1e1
alfan-ntu <maoyi_fan@yahoo.com.tw>
2016-10-01 19:45:41 +0800

    Demonstrates how to use $_SERVER global variables

--- demo0404.php
+++ demo0404.php
@@ -3,6 +3,12 @@ $shop_name = "Al's Cafe Shop";
 // demonetrates how to make a call to a function
 welcome();
 
+// a few example of PHP GLOBAL VARIABLES
+echo "Your browser is {$_SERVER['HTTP_USER_AGENT']} <br>";
+echo "Full path name of this PHP file is {$_SERVER['SCRIPT_FILENAME']} <br>";
+echo "The way how request is sent : {$_SERVER['REQUEST_METHOD']} <br>";
+echo "The request sent is {$_SERVER['REQUEST_URI']} <br>";
+
 // demonstrates how to define a function 
 function welcome()
 {
@@ -10,7 +16,7 @@ function welcome()
 global $user_name, $shop_name;
 // changed _GET or _POST to _REQUEST, this way, 
 // it can take variables passed in through either _GET or _POST
-echo "Hello {$_POST['user_name']}，Welcome to {$shop_name}";
+echo "Hello {$_POST['user_name']}, Welcome to {$shop_name} <br>";
 }
 
 ?>
