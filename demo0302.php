d74d19eac0391bb2b6f02d8407808a227e3fc4d9
alfan-ntu <maoyi_fan@yahoo.com.tw>
2016-10-01 18:35:57 +0800

    Show use of PHP variables

---
+++ demo0302.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Example demo0302</title>
+  </head>
+  <body>
+    <h1>Tad deserts bar</h1>
+    <?php
+//
+// The following statement shows how a variable is used
+//
+      echo "Welcome to $shopName ";
+      echo "Time now ", date("Y-m-d H:i:s");
+      phpinfo();
+    ?>
+  </body>
+</html>
