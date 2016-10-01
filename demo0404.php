d06a95ca23bd490e1dc200332aa3636c0b3d4840
alfan-ntu <maoyi_fan@yahoo.com.tw>
2016-10-01 19:24:05 +0800

    Demonstrates function and memory space of variables

--- demo0404.php
+++ demo0404.php
@@ -1,5 +1,16 @@
 <?php
 $shop_name = "Al's Cafe Shop";
+// demonetrates how to make a call to a function
+welcome();
+
+// demonstrates how to define a function 
+function welcome()
+{
+// demonstrates how to use 'global' to extend the variable validity space
+global $user_name, $shop_name;
 // changed _GET or _POST to _REQUEST, this way, 
 // it can take variables passed in through either _GET or _POST
-echo "Hello {$_REQUEST['user_name']}，Welcome to {$shop_name}";
+echo "Hello {$_POST['user_name']}，Welcome to {$shop_name}";
+}
+
+?>
